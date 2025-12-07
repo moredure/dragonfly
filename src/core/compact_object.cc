a06d0c0c3b517772804b99c98bb7a1008c2a00dc
Mykhailo Faraponov <11322032+moredure@users.noreply.github.com>
2025-12-07 23:05:58 +0200

    chore(core): Refactor ExtendExisting for better prepend handling
    Optimize ExtendExisting function for prepend operation.
    
    Signed-off-by: Mykhailo Faraponov
    <11322032+moredure@users.noreply.github.com>

--- src/core/compact_object.cc
+++ src/core/compact_object.cc
@@ -562,8 +562,13 @@ void RobjWrapper::ReserveString(size_t size, MemoryResource* mr) {
 }
 
 void RobjWrapper::AppendString(string_view s, MemoryResource* mr) {
+  type_ = OBJ_STRING;
+  encoding_ = OBJ_ENCODING_RAW;
+
   size_t cur_cap = InnerObjMallocUsed();
-  CHECK(cur_cap >= sz_ + s.size()) << cur_cap << " " << sz_ << " " << s.size();
+  if (cur_cap < sz_ + s.size()) {
+    MakeInnerRoom(cur_cap, sz_ + s.size(), mr);
+  }
   memcpy(reinterpret_cast<uint8_t*>(inner_obj_) + sz_, s.data(), s.size());
   sz_ += s.size();
 }
